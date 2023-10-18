4baae959d5ec431829a1c7493fbe74bb076e80be
princebillygk <princebillygk@gmail.com>
2023-10-18 18:03:26 +0600

    started dummy  server

--- cmd/chatbot/main.go
+++ cmd/chatbot/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Received webhook:")
+		fmt.Println(r.Body)
+		w.WriteHeader(200)
+		w.Write([]byte("hello"))
+	})
+
+	fmt.Println("Started http server...")
+	http.ListenAndServe(":3000", nil)
 }
