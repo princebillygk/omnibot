bee18c6699af2f546b3aefd01f66b226e91e6c26
princebillygk <princebillygk@gmail.com>
2023-10-22 14:29:57 +0600

    successfully connected messenger with my webhook

--- cmd/internal/controller/chat.go
+++ cmd/internal/controller/chat.go
@@ -1,21 +1,35 @@
 package controller
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
+
+var msngrVerfToken string
+
+func init() {
+	var ok bool
+	msngrVerfToken, ok = os.LookupEnv("MESSENGER_WEBHOOK_INTEGRATION_TOKEN")
+	if !ok {
+		panic("Messenger Verification Webhook doesn't exists")
+	}
+}
 
 // Chat is a controller for messaging services
 type Chat struct {
 }
 
 func (c Chat) HandleMessenger(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Body)
+
 	query := r.URL.Query()
 	mode, token, challenge := query.Get("hub.mode"), query.Get("hub.verify_token"), query.Get("hub.challenge")
 
-	if mode == "subscribe" && token == "myToken" {
+	if mode == "subscribe" && token == msngrVerfToken {
 		w.WriteHeader(200)
-		fmt.Fprintf(w, "%s %s %s\n", mode, token, challenge)
+		w.Write([]byte(challenge))
+		return
 	}
 	w.WriteHeader(http.StatusForbidden)
 }
